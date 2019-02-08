82969a4aa0de5cf62a8a687072bad64e26315eec
ahsanhabib91 <habibibnakther@gmail.com>
2019-02-08 18:02:18 +0600

    started working on form

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -11,7 +11,18 @@ import {
 
 class Register extends React.Component {
   render() {
-    return <div>Register</div>;
+    return (
+      <div>
+        <Grid textAlign="center" verticalAlign="middle">
+          <Grid.Column style={{ maxWidth: 450 }}>
+            <Header as="h2" icon color="orange" textAlign="center">
+              <Icon name="puzzle piece" color="orange" />
+              Register for DevChat
+            </Header>
+          </Grid.Column>
+        </Grid>
+      </div>
+    );
   }
 }
 export default Register;
